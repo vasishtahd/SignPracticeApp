3aebf47b98e20cc3264bda4518a2f694749a9476
vasishtahd <30791332+vasishtahd@users.noreply.github.com>
2019-10-06 23:53:16 -0700

    Basic Video Record And Play

---
+++ SignPracticeApp/LearnSign/app/src/main/java/com/example/learnsign/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.learnsign;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static int VIDEO_REQUEST=101;
+    private Uri videoUri=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playVideo(View view) {
+
+        Intent videoIntent=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+        if(videoIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(videoIntent,VIDEO_REQUEST);
+
+
+        }
+
+    }
+
+    public void captureVideo(View view) {
+        Intent playIntent=new Intent(this, VideoPlayActivity.class);
+        playIntent.putExtra("videoUri",videoUri.toString());
+        startActivity(playIntent);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==VIDEO_REQUEST && resultCode==RESULT_OK){
+            videoUri=data.getData();
+        }
+    }
+}
